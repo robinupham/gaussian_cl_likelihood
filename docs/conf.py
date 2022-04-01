07e69b01ad670674df385bed1773385860d2c0e0
Robin Upham <rupham1@gmail.com>
2022-04-01 14:08:30 +0100

    Add scipy and pseudo_cl_likelihood to mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../python'))
-autodoc_mock_imports = ['healpy', 'matplotlib', 'npeet', 'numpy']
+autodoc_mock_imports = ['healpy', 'matplotlib', 'npeet', 'numpy', 'pseudo_cl_likelihood', 'scipy']
 
 
 # -- Project information -----------------------------------------------------
